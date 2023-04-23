aa2dc48c88658b8c8e3052b95dc45849211afa73
Majid Igale <majidigale18@gmail.com>
2023-04-23 08:22:57 -0500

    Just created the listItems function and looped it through the basket array

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -4,9 +4,21 @@ console.log('***** Cart Functions *****');
 let basket = []
 console.log('We set basket array');
 function additem(item){
-    console.log('his is what we have picked up:',item );
+    console.log('This is what we have picked up:',item );
 basket.push(item);
 return basket;
 }
 additem('Banana');
 additem('Apple');
+additem('Melon');
+additem('Cake');
+
+function listItems(){
+for(i=0; i<basket.length;i++){
+    console.log('What do we got in our basket?:', basket[i]);
+    console.log(basket[0]);
+    console.log(basket[1]);
+    console.log(basket[2]);
+    console.log(basket[3]);
+}
+}
