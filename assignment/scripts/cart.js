53ed568950b9629512b1cce5c841484a3b192fa1
Majid Igale <majidigale18@gmail.com>
2023-04-23 07:48:50 -0500

    figured out how to set up function 1

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,12 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = []
+console.log('We set basket array');
+function additem(item){
+    console.log('his is what we have picked up:',item );
+basket.push(item);
+return basket;
+}
+additem('Banana');
+additem('Apple');
